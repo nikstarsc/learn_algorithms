b680a388f8b2cc7d0f9b0ffa5182625eeba5454d
nikstarsc <nikstarsc@gmail.com>
2020-10-24 15:34:56 +0530

    Refactored .h file

--- src/include/algo_lib.h
+++ src/include/algo_lib.h
@@ -8,8 +8,7 @@
 
 void insertion_sort_increasing(int* array_to_sort, int length_of_array);
 void insertion_sort_decreasing(int* array_to_sort, int length_of_array);
-void print_array_before_and_after_sort (void (*foo)(int*, int),
-										int* array_to_sort, int length_of_array);
+void print_array_before_and_after_sort (void (*foo)(int*, int), int* array_to_sort, int length_of_array);
 
 
 #endif // ALGO_LIB_H_INCLUDED
