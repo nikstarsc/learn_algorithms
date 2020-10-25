dadcab96d5003f287932bf3a85d36fc8ed3a2451
nikstarsc <nikstarsc@gmail.com>
2020-10-25 18:46:28 +0530

    Refactored utilities.c

--- src/utilities.c
+++ src/utilities.c
@@ -6,6 +6,7 @@ Various Utilities functions defined in this file.
 
 void print_array(int* array_to_sort, int length_of_array)
 {
+	printf("[");
 	for(int i = 0; i < length_of_array; i++)
 	{
 		printf((i < (length_of_array - 1)) ? "%d, ": "%d]\n",
@@ -16,11 +17,11 @@ void print_array(int* array_to_sort, int length_of_array)
 
 void print_array_before_and_after_sort (void (*foo)(int*, int), int* array_to_sort, int length_of_array)
 {
-	printf("array before applying sort =\t[");
+	printf("array before applying sort =\t");
 	print_array(array_to_sort, length_of_array);
 	foo(array_to_sort, length_of_array);
 
-	printf("array after applying sort =\t[");
+	printf("array after applying sort =\t");
 	print_array(array_to_sort, length_of_array);
 }
 
