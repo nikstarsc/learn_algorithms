38fd015ef9f660febe61a651a484b0fe9494f084
nikstarsc <nikstarsc@gmail.com>
2020-10-25 18:41:50 +0530

    Refactor code in utilities.c

--- src/selection_sort.c
+++ src/selection_sort.c
@@ -3,8 +3,8 @@ Algorithm for selection sort for question 2.2.2 from introduction to algorithm
 */
 
 #include "include/algo_lib.h"
-#define increasing(i, j) *(array_to_sort + j) < *(array_to_sort + i)
-#define decreasing(i, j) *(array_to_sort + j) > *(array_to_sort + i)
+#define increasing(i, j) (*(array_to_sort + j) < *(array_to_sort + i))
+#define decreasing(i, j) (*(array_to_sort + j) > *(array_to_sort + i))
 
 static void sort_array_in_given_order(bool increasing_order, int* array_to_sort, int length_of_array)
 {
--- src/utilities.c
+++ src/utilities.c
@@ -4,22 +4,23 @@ Various Utilities functions defined in this file.
 
 #include "include/algo_lib.h"
 
+void print_array(int* array_to_sort, int length_of_array)
+{
+	for(int i = 0; i < length_of_array; i++)
+    {
+		printf((i < (length_of_array - 1)) ? "%d, ": "%d]\n",
+			  *(array_to_sort + i));
+    }
+
+}
+
 void print_array_before_and_after_sort (void (*foo)(int*, int), int* array_to_sort, int length_of_array)
 {
 	printf("array before applying sort =\t[");
-    for(int i = 0; i < length_of_array; i++)
-    {
-		printf((i < (length_of_array - 1)) ? "%d, ": "%d]\n",
-			  *(array_to_sort + i));
-    }
-
+    print_array(array_to_sort, length_of_array);
     foo(array_to_sort, length_of_array);
 
     printf("array after applying sort =\t[");
-    for(int i = 0; i < length_of_array; i++)
-    {
-		printf((i < (length_of_array - 1)) ? "%d, ": "%d]\n",
-			  *(array_to_sort + i));
-    }
+    print_array(array_to_sort, length_of_array);
 }
 
